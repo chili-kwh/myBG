9dde15c25889fd2fa90062704fa91c675825b584
JosefinaLee <714448530@qq.com>
2017-03-27 16:08:12 +0800

    webpack

--- src/js/reactTest.js
+++ src/js/reactTest.js
@@ -7,14 +7,30 @@ import React from "react";
 import content from "../content.json";
 
 class Test extends React.Component {
+
+    pushMe(...args) {
+        console.log(...args);
+        console.log(args);
+    }
+
     render() {
         return (
             <div>
-                <div className="test">{content.greetText}富商大</div>
-                <div className="testPic">efarfcdsfewfdsaffsfdsadsfa</div>
+                <div className="test">{content.greetText}富dd商大</div>
+                <div className="testPic"
+                    onClick={()=>{
+                        this.pushMe({}, {s:4, r:4})
+                    }}
+                >
+                    efarfcdsfdddewfdsaffsfdsadsfa
+                    快给他喝{
+                    content.greetText}
+                </div>
             </div>
         )
     }
+
+
 }
 
 export {
